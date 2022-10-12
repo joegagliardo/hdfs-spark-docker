ab909ed0e1bee5e07f3738e76912f37b825205db
Joseph Gagliardo <joey@me.com>
2022-10-11 20:50:36 -0500

    0.0.7

--- install-docker.sh
+++ install-docker.sh
@@ -2,8 +2,8 @@
 apt update
 apt install apt-transport-https curl gnupg-agent ca-certificates software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
-apt install docker-ce docker-ce-cli containerd.io -y
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable" -y
+apt install docker-ce docker-ce-cli containerd.io docker-compose -y
 usermod -aG docker $USER
 newgrp docker
 systemctl start docker
