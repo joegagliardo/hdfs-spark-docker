7501986976ee6f77ce6c484d3ebade67b07f5e16
Joseph Gagliardo <joey@me.com>
2022-10-11 22:27:09 -0500

    0.1.0

--- install-docker.sh
+++ install-docker.sh
@@ -6,9 +6,9 @@ apt install net-tools apt-transport-https curl gnupg-agent ca-certificates softw
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable" -y
 apt install docker-ce docker-ce-cli containerd.io docker-compose -y
-sudo usermod -aG docker $USER
-newgrp docker
 systemctl start docker
 systemctl enable docker
-cd ~/hdfs-spark-docker
-docker-compose up -d
+sudo usermod -aG docker $USER
+newgrp docker
+# cd ~/hdfs-spark-docker
+# docker-compose up -d
