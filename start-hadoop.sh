ab4f69f9fe5cad5e32efd0bef4c9615a487b06f2
Joseph Gagliardo <joey@me.com>
2022-10-20 19:08:00 -0500

    0.1.4

--- start-hadoop.sh
+++ start-hadoop.sh
@@ -1,19 +1,25 @@
 #! /bin/sh
-MODE="${1:-3}"
+# MODE="${1:-3}"
 
-if [ "$MODE" == "4" ] ; then 
-    echo "Start hadoop with 4 nodes"
-    cp ./docker-compose-hadoop-4.yaml docker-compose-hadoop.yaml
-elif [ "$MODE" == "5" ] ; then 
-    echo "Start hadoop with 5 nodes"
-    cp ./docker-compose-hadoop-5.yaml docker-compose-hadoop.yaml
-elif [ "$MODE" == "6" ] ; then 
-    echo "Start hadoop with 6 nodes"
-    cp ./docker-compose-hadoop-6.yaml docker-compose-hadoop.yaml
-else 
-    echo "Start hadoop with 3 nodes"
-    cp ./docker-compose-hadoop-3.yaml docker-compose-hadoop.yaml
-fi
+# if [ "$MODE" == "4" ] ; then 
+#     echo "Start hadoop with 4 nodes"
+#     cp ./docker-compose-hadoop-4.yaml docker-compose-hadoop.yaml
+# elif [ "$MODE" == "5" ] ; then 
+#     echo "Start hadoop with 5 nodes"
+#     cp ./docker-compose-hadoop-5.yaml docker-compose-hadoop.yaml
+# elif [ "$MODE" == "6" ] ; then 
+#     echo "Start hadoop with 6 nodes"
+#     cp ./docker-compose-hadoop-6.yaml docker-compose-hadoop.yaml
+# else 
+#     echo "Start hadoop with 3 nodes"
+#     cp ./docker-compose-hadoop-3.yaml docker-compose-hadoop.yaml
+# fi
 
-docker-compose -f ./docker-compose-hadoop.yaml up -d
+# docker-compose -f ./docker-compose-hadoop.yaml up -d
+
+
+#! /bin/sh
+NODES="${1:-${NODES:-3}}"
+echo "Starting hadoop with $NODES nodes."
+docker-compose -f ./docker-compose-hadoop-$NODES.yaml up -d
 
